5ea8e4af750ccf32d091c7cb3586657980e53c56
Nikita <skwaen@gmail.com>
2019-12-04 20:35:17 +0200

    interpol

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log('was loaded');
+
+let str1 = 'text 111';
+let str2 = 'text 222';
+let str3 = 'text 333';
+
+console.log('text 1 : text 2');
+console.log(str1 + ' : ' + str2 + ' # ' + str3);
+console.log(`${str1} : ${str2} # ${str3}`);
